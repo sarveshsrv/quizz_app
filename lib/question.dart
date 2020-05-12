17392c5a4de036c7be6e3be6dba3e2f42ce0df7b
Sarvesh Tiwari <sarveshsandilya01@gmail.com>
2020-05-12 15:01:08 +0530

    Updated questions.dart

--- lib/question.dart
+++ lib/question.dart
@@ -12,10 +12,9 @@ class Question extends StatelessWidget {
       margin: EdgeInsets.all(10),
       child: Text(
         questionText,
-        style: TextStyle(fontSize: 20),
+        style: TextStyle(fontSize: 28),
         textAlign: TextAlign.center,
       ),
-    )
-    ;
+    );
   }
 }
