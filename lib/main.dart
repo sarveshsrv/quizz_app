ff9d409bb4423c7aa082c1e661c2a13f12402cb2
Sarvesh Tiwari <sarveshsandilya01@gmail.com>
2020-05-15 01:41:57 +0530

    Rectangular border added

--- lib/main.dart
+++ lib/main.dart
@@ -79,8 +79,11 @@ class _MyAppState extends State<MyApp> {
             : Center(
                 child: RaisedButton(
                   color: Colors.redAccent,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50.0)
+                  ),
                   textColor: Colors.white,
-                  child: Text("You did it.No more questions!"),
+                  child: Text("You did it!", style: TextStyle(fontWeight: FontWeight.bold),),
                   onPressed:(){},
                 ),
               ),
