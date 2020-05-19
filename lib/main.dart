6dbfa8271a49bd907c503b7b1f9db7156071d0ad
Sarvesh Tiwari <sarveshsandilya01@gmail.com>
2020-05-20 01:57:51 +0530

    Reset quiz property added

--- lib/main.dart
+++ lib/main.dart
@@ -18,24 +18,25 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   final _questions = const [
-    {
+     {
       'questionText': 'What\'s your favorite color?',
       'answers': [
-        {'text': 'Black', 'score': 5},
-        {'text': 'White', 'score': 7},
-        {'text': 'Blue', 'score': 4},
-        {'text': 'Green', 'score': 10}
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1},
       ],
     },
     {
       'questionText': 'What\'s your favorite animal?',
       'answers': [
-        {'text': 'Rabbit', 'score': 5},
-        {'text': 'Snake', 'score': 7},
-        {'text': 'Elephant', 'score': 8},
-        {'text': 'Lion', 'score': 10}
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Snake', 'score': 11},
+        {'text': 'Elephant', 'score': 5},
+        {'text': 'Lion', 'score': 9},
       ],
     },
+
     {
       'questionText': 'Who\'s your favorite instructor?',
       'answers': [
@@ -48,6 +49,13 @@ class _MyAppState extends State<MyApp> {
   ];
   var _questionIndex = 0;
   var _totalScore = 0;
+
+   void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
 
   void _answerQuestion(int score) {
     // var aBool = true;
@@ -84,7 +92,7 @@ class _MyAppState extends State<MyApp> {
                 questionIndex: _questionIndex,
                 questions: _questions,
               )
-            : Result(_totalScore),
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
--- lib/result.dart
+++ lib/result.dart
@@ -1,21 +1,38 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+   final int resultScore;
+  final Function resetHandler;
+
+  Result(this.resultScore, this.resetHandler);
+  
+  String get resultPhrase{
+  String resultText;
+  if(resultScore<=10){
+    resultText="You are nice and innocent!";
+  }
+  else if(resultScore>10 && resultScore<=15){
+    resultText="You are pretty likeable!";
+  }
+  else {
+    resultText="You are bad!";
+  }
+  return resultText;
+  }
   @override
   Widget build(BuildContext context) {
     return Center(
-                child: RaisedButton(
-                  color: Colors.redAccent,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50.0)),
-               
-                  textColor: Colors.white,
-                  child: Text(
-                    "You did it!",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  onPressed: () {},
-                ),
-              );
+      child: RaisedButton(
+        color: Colors.redAccent,
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(50.0)),
+        textColor: Colors.white,
+        child: Text(
+          resultPhrase,
+          style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+        ),
+        onPressed: () {},
+      ),
+    );
   }
 }
